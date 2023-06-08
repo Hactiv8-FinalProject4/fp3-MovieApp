a0346aaf57f1c2fb5642dfb704cf528040a62c00
Dani Rizky Zaelani <danirizky704@gmail.com>
2023-06-09 02:10:24 +0700

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='w-full bg-slate-950'>
+    <h1 className='text-white text-left p-4'>Movie App</h1>
+    </div>
+  )
+}
+
+export default index
