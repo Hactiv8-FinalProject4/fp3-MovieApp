eac69bb75ea1004c2355d3d4f8b97901549f9b67
Dani Rizky Zaelani <danirizky704@gmail.com>
2023-06-11 20:41:23 +0700

    feat: add navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "../src/components/Header";
+import Footer from "../src/components/Footer"
 import Home from "../src/Pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
